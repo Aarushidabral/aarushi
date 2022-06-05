e96501bcf6f8a78e41631b7051f51d4bd9d99d33
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-05 15:13:28 +0530

    Create 35.py

---
+++ 35.py
@@ -0,0 +1,7 @@
+#natural no. in reverse order
+num=int(input("enter any no.:"))
+i=num
+print("list the natural no. from {0} to 1 in reverse order:".format(num))
+while(i>=1):
+    print(i,end='')
+    i=i-1
