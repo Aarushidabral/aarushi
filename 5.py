ed4af8adcc6ddd8926b992b19ef996ca99716bdb
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-05 14:00:29 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,18 @@
+#arithmetic operation
+num1=float(input("enter the first no.:"))
+num2=float(input("enter the second no.:"))
+#sum
+add=num1+num2
+#subtract
+sub=num1-num2
+#multiply
+mul=num1*num2
+#divide
+div=num1/num2
+#modulus
+mod=num1%num2
+print("the value of addition is:",add)
+print("the value of subtraction is:",sub)
+print("the value of multipication is:",mul)
+print("the value of divide is:",div)
+print("the value of modulus is:",mod)
