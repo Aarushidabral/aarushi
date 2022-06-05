5eb5a1b2c7ab8affdace5a95e60985e9cab0b94e
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-05 14:53:12 +0530

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,6 @@
+#check divisibility of 5 &11
+num=float(input("enter the no.:"))
+if((num%5==0)and(num%11==0)):
+    print("the no. {0} is divible by 5 and 11 both".format(num))
+else:
+    print("no. {0} is not divisible by 5 & 11")    
