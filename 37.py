2e629e1b7e3425b9336806278de8f8bc185e6253
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-05 15:14:56 +0530

    Create 37.py

---
+++ 37.py
@@ -0,0 +1,9 @@
+#avg of two no.
+a=int(input("enter the first no.:"))
+b=int(input("enter the seond no.:"))
+sum=a+b
+avg=(a+b)/2
+flooravg=(a+b)//2
+print('the sum of {0} and {1} ={2}'.format(a,b,sum))
+print('the avrage of {0} and {1}={2}'.format(a,b,avg))
+print('the floor average of {0} and {1}={2}'.format(a,b,flooravg))
