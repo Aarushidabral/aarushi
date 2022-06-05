b8b9624f4d871672fd99585850e9470b1db5a92f
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-05 15:14:10 +0530

    Create 36.py

---
+++ 36.py
@@ -0,0 +1,8 @@
+#sum and avg of 3 no.s
+a=int(input("enter the first no.:"))
+b=int(input("enter the second no.:"))
+c=int(input("enter the third no.:"))
+sum=a+b+c
+avg=sum/3
+print('the sum of {0},{1},{2}={3}'.format(a,b,c,sum))
+print('the avgerage of {0},{1},{2}={3}'.format(a,b,c,avg))
