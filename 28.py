f83e94d9da2e311257e33ccee30322ce0bc919c1
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-05 15:04:47 +0530

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,6 @@
+#simple interest
+p=float(input("enter the principle amount:"))
+r=float(input("enter the rate of interest:"))
+t=float(input("enter the time period:"))
+simple_interest=(p*r*t)/100
+print("\n simple inerest for {0}={1}".format(p,simple_interest))
