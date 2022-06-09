c3d0157aa7467f0b4c90676705311929cb461039
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-09 17:19:41 +0530

    Create C33.py

---
+++ C33.py
@@ -0,0 +1,8 @@
+#swap two no.
+a=float(input("enter the first value a:"))
+b=float(input("enter the scond value b:"))
+print("before swapping two number:a={0} and b={1}".format(a,b))
+temp=a
+a=b
+b=temp
+print("after swapping two number: a={0} and b={1}".format(a,b))
