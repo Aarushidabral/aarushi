74ae62b80562b3297ed85bd55c176877e39d9d9c
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-09 17:31:34 +0530

    Create D5.py

---
+++ D5.py
@@ -0,0 +1,6 @@
+#check digit or not
+ch=input("enter your own character:")
+if(ch>='0' and ch<='9'):
+    print("the given character ",ch,"is a digit")
+else:
+    print("the given character ",ch,"is not a digit")
