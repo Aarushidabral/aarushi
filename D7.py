ee2259ebc77e7e75f1c4865677039b88f3f46261
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-09 17:43:13 +0530

    Create D7.py

---
+++ D7.py
@@ -0,0 +1,6 @@
+#uppercase or not
+ch=input("enter your own character:")
+if(ch.isupper()):
+    print("the given character",ch,"is a uppercase alphabet")
+else:
+    print("the given character",ch,"is not uppercase alphabet")
