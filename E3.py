f1235e0c55c559e3594483d1d36d301d01fdba09
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-09 17:45:57 +0530

    Create E3.py

---
+++ E3.py
@@ -0,0 +1,7 @@
+#to concatenate string
+str1=input("enter the first string:")
+str2=input("enter the second string:")
+concat1=str1+str2
+print("the final string =",concat1)
+concat2=str1 + ' ' + str2
+print("final string with space=",concat2)
