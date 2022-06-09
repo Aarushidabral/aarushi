9b49bcc286a9081a96b2d44ae3ba6c5b98b69d59
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-09 17:08:02 +0530

    Create C28.py

---
+++ C28.py
@@ -0,0 +1,11 @@
+#find prime factor of no.
+num=int(input("enter any no.:"))
+for i in range(2,num+1):
+    if(num%i==0):
+        isprime=1
+        for j in range(2,(i//2+1)):
+            if(i%j==0):
+                isprime=0
+                break
+        if(isprime==1):
+            print("%d is prime factorof a given no.%d"%(i,num))
