2b38dd12407a9a99be64fe8b6d25955ee68834ea
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-09 17:20:53 +0530

    Create D1.py

---
+++ D1.py
@@ -0,0 +1,6 @@
+#to check alphabet or no.
+ch=input("enter your character:")
+if((ch>='a' and ch<='z')or(ch>='A' and ch<='Z')):
+    print("the given character",ch,"is an Alphabet")
+else:
+    print("the given character",ch,"is not an Alphabet")
