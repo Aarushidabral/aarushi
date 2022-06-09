f6dc6d4bf23554e97679feef370a337d1aae1a72
Aarushidabral <106898468+Aarushidabral@users.noreply.github.com>
2022-06-09 17:43:52 +0530

    Create D8.py

---
+++ D8.py
@@ -0,0 +1,6 @@
+check vowel or constant
+ch=input("enter your own character:")
+if(ch=='a'or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='u'):
+    print("the given character",ch,"is a vowel")
+else:
+    print("the given character",ch,"is  consonant")
